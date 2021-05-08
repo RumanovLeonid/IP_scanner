6e3277bed4ea9a082c8e1fc8dd0c48ea9768516c
Rumanov Leonid <rumanovleonid@hotmail.com>
2021-05-09 01:45:25 +0300

    Вроде, побыстрей.

--- Program.cs
+++ Program.cs
@@ -4,33 +4,46 @@ using System.Net.NetworkInformation;
 
 class IP_Scaner
 {
+    static Ping ping = new Ping();
+    static PingReply pingReply = null;
+
     public static void Main()
     {
-        //Thread tread_1_to_125 = new Thread();
-        //Thread tread_126_to_254 = new Thread();
-
         Console.WriteLine("Активные устройства в подсети:");
+
         for (int i = 1; i <= 254; i++)
         {
             string ipnum = "192.168.1." + i;
             PingCheck(ipnum);
         }
+
         Console.WriteLine("Сканирование завершено");
 
         Console.ReadKey();
     }
 
+    public static void ScanIPRange(int BeginRange, int EndRange)
+    {
+        for (int i = BeginRange; i <= EndRange; i++)
+        {
+            string ipnum = "192.168.1." + i;
+            PingCheck(ipnum);
+        }
+    }
+
     public static void PingCheck(string A)
     {
-        Ping ping = new Ping();
-        PingReply pingReply = null;
-        string server = A;
-        pingReply = ping.Send(server);
+        pingReply = ping.Send(A, 5);
+
         if (pingReply.Status == IPStatus.Success)
         {
-            Console.WriteLine(server);
-            Console.WriteLine(pingReply.Status);
-            Console.WriteLine();
+            Console.Write(A);
+            Console.WriteLine(" " + pingReply.Status);
+        }
+        else
+        {
+            Console.Write(A);
+            Console.WriteLine(" "+ IPStatus.NoResources);
         }
     }
 }
